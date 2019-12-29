118d26cfaa5ddc9a65e7d1157a8c816aad5be683
Nelson Mwirumubi <mwinel@live.com>
2019-12-29 11:02:13 +0300

    Initial commit

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,9 +1,11 @@
 import React from "react";
+import logo from '../logo.png'
 
 const Default = () => {
   return (
     <div className="container my-5">
       <h1>koffie time app</h1>
+      <img src={logo} alt="logo" width={40}/>
     </div>
   );
 };
